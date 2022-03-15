4d1e2dbcbd68e51f947b29986893e08603aba80b
Kho Van Kiryl <van_kiryl_kho@epam.com>
2022-03-15 15:50:02 +0300

    Create primary BaseTest class

---
+++ TestLayer/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace TestLayer
+{
+    public class BaseTest
+    {
+        protected IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver?.Close();
+        }
+    }
+}
