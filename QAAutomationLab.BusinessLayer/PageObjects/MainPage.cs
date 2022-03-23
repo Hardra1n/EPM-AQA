27cc6c39bbe90bcb6fdac02177dd0f26f5175309
EugenTrifonov <trifonov-15@mail.ru>
2022-03-23 22:45:46 +0300

    Add main page

---
+++ QAAutomationLab.BusinessLayer/PageObjects/MainPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.BasePage;
+
+namespace QAAutomationLab.BusinessLayer.PageObjects
+{
+    public class MainPage:BasePage
+    {
+        private const string _title = "Booking.com";
+
+        public MainPage(IWebDriver driver):base(driver) 
+        {
+            Driver.FindElement(By.XPath($"//title[contains(text(),'{_title})]"));
+        }
+    }
+}
