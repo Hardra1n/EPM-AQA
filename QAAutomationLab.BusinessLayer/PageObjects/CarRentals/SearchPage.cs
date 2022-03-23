b78715fb5861ccb7466fbadde11a7f987f7af1a2
EugenTrifonov <trifonov-15@mail.ru>
2022-03-23 23:40:37 +0300

    Add Search page

---
+++ QAAutomationLab.BusinessLayer/PageObjects/CarRentals/SearchPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.BasePage;
+using QAAutomationLab.CoreLayer.WebElement;
+
+namespace QAAutomationLab.BusinessLayer.PageObjects.CarRentals
+{
+    public class SearchPage:BasePage
+    {
+        private const string _title = "car hire";
+
+        public BaseWebElement SameLocationSelector = new BaseWebElement(By.XPath("//label[contains(text(),'same location')]"));
+
+        public BaseWebElement DifferentLocationSelector = new BaseWebElement(By.XPath("//label[contains(text(),'different location')]"));
+
+        public BaseWebElement SearchField = new BaseWebElement(By.XPath("//input[contains(@placeholder,'Pick-up location')]"));
+
+        public BaseWebElement SearchButton = new BaseWebElement(By.XPath("//span[contains(text(),'Search')]/.."));
+
+        public SearchPage(IWebDriver driver):base(driver)
+        {
+            Driver.FindElement(By.XPath($"//title[contains(text(),'{_title}')]"));
+        }
+    }
+}
