a18d33a4e40e57689c79c4bb9342d17c20815d27
EugenTrifonov <trifonov-15@mail.ru>
2022-03-25 15:35:23 +0300

    Add search results page

---
+++ QAAutomationLab.BusinessLayer/PageObjects/CarRentals/SearchResultsPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.BasePage;
+
+namespace QAAutomationLab.BusinessLayer.PageObjects.CarRentals
+{
+    public class SearchResultsPage:BasePage
+    {
+        private const string _title = "at discounted rates";
+
+        public SearchResultsPage() : base() 
+        {
+            DriverInstance.FindElement(By.XPath($"//title[contains(text(),'{_title}')]"));
+        }
+    }
+}
