25be2006505ba16548acd186373a4d43c4141832
EugenTrifonov <trifonov-15@mail.ru>
2022-03-28 10:48:58 +0300

    Add search with results test

---
+++ TestLayer/CarRentals/CarRentalsPageTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.PageObjects.CarRentals;
+using QAAutomationLab.BusinessLayer.Utilities;
+
+namespace TestLayer.CarRentals
+{
+    [TestFixture]
+    public class CarRentalsPageTests:BaseTest
+    {
+        [Test]
+        public void SearchWithExistingResults() 
+        {
+            var page = Utilities.RunBrowser("www.booking.com")
+                 .GoToCarRentals()
+                 .ChooseSameLocation()
+                 .EnterPickUpLocation("Berlin")
+                 .ChooseFirstSearchSuggestion().ClickSearchButton();
+
+            Assert.IsTrue(page is SearchResultsPage);
+        }
+    }
+}
