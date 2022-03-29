e6975c76617bd7312c17b81d25a7053b603f5ee7
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-03-30 01:48:34 +0300

    Add base structure to Searching page tests

--- TestLayer/Stays/SearchingPageTests.cs
+++ TestLayer/Stays/SearchingPageTests.cs
@@ -1,15 +1,33 @@
 ﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.PageObjects.Stays;
+using QAAutomationLab.BusinessLayer.Utilities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TestLayer.Stays
 {
     [TestFixture]
     public class SearchingPageTests : BaseTest
     {
+        private StaysSearchingPage _page;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _page = Utilities.RunBrowser(TestsSettings.MainPageUrl).GoToStays();
+        }
+
+        [TestCase("Warsaw")]
+        public void CorrectStaysSearchingTest(string destination)
+        {
+            DateTime stayFromDate = DateTime.Now;
+            DateTime stayToDate = DateTime.Now.AddDays(1);
+
+            _page.EnterDestination(destination)
+                 .ClickCalendarMenu()
+                 .SelectDatesToStay(stayFromDate, stayToDate);
+            
+            Assert.Fail("Not fully implemented yet");
+        }
     }
 }
