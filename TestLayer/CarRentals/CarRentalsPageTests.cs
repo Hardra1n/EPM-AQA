e8bc54a06518c2614da46fe5736cf217ed4f7ce7
EugenTrifonov <trifonov-15@mail.ru>
2022-03-30 14:27:24 +0300

    Add some tests

--- TestLayer/CarRentals/CarRentalsPageTests.cs
+++ TestLayer/CarRentals/CarRentalsPageTests.cs
@@ -5,18 +5,53 @@ using QAAutomationLab.BusinessLayer.Utilities;
 namespace TestLayer.CarRentals
 {
     [TestFixture]
-    public class CarRentalsPageTests:BaseTest
+    public class CarRentalsPageTests : BaseTest
     {
         [Test]
-        public void SearchWithExistingResults() 
+        [TestCase("Berlin")]
+        public void SearchWithExistingResults(string location)
         {
-            var page = Utilities.RunBrowser("www.booking.com")
+            bool result = Utilities.RunBrowser(TestsSettings.MainPageUrl)
                  .GoToCarRentals()
                  .ChooseSameLocation()
-                 .EnterPickUpLocation("Berlin")
-                 .ChooseFirstSearchSuggestion().ClickSearchButton();
+                 .EnterPickUpLocation(location)
+                 .ChooseFirstPickUpSuggestion(location)
+                 .ClickSearchButton()
+                 .IsNoResultsMessageShown();
 
-            Assert.IsTrue(page is SearchResultsPage);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        [TestCase("ase232")]
+        public void InvalidSearchRequest(string location)
+        {
+            var page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                .GoToCarRentals()
+                .ChooseSameLocation()
+                .EnterPickUpLocation(location);
+
+            Assert.Throws<OpenQA.Selenium.NoSuchElementException>(() => page.ClickSearchButton());
+
+            bool vissible = page.IsErrorMessageShown();
+
+            Assert.IsTrue(vissible);
+        }
+
+        [Test]
+        [TestCase("Pruzhany","Los Angeles")]
+        public void SearchWithoutExistingResults(string pickUpLocation,string dropDownLocation) 
+        {
+            bool result = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                .GoToCarRentals().ChooseDifferentLocation()
+                .EnterPickUpLocation(pickUpLocation)
+                .ChooseFirstPickUpSuggestion(pickUpLocation)
+                .EnterDropOffLocation(dropDownLocation)
+                .ChooseFirstDropOffSuggestion(dropDownLocation)
+                .ClickSearchButton()
+                .IsNoResultsMessageShown();
+
+            Assert.IsTrue(result);
         }
     }
 }
