d3190c902d5de60d4a2e9384fb2663acd545ad4d
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-03-31 12:53:48 +0300

    Add testing navigating to ad page

---
+++ TestLayer/Stays/StaysSearchResultsTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.Models;
+using QAAutomationLab.BusinessLayer.PageObjects;
+using QAAutomationLab.BusinessLayer.PageObjects.Stays;
+using QAAutomationLab.BusinessLayer.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestLayer.Stays
+{
+    [TestFixture]
+    public class StaysSearchResultsPageTests : BaseTest
+    {
+        private StaysSearchResultsPage _page;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                             .GoToStays()
+                             .AddSearchingContext(StaysSearchingContext.GetDefaultContext())
+                             .ClickSearchButton();
+        }
+
+        [Test]
+        public void CorrectNavigatingToAdPage()
+        {
+            string expectedAdPageTitleSubstring = _page.GetFirstAddTitle();
+
+            string actualAdPageTitle = _page.ClickFirstAdNavigatingButton().GetHotelName();
+
+            Assert.That(actualAdPageTitle, Does.Contain(expectedAdPageTitleSubstring));
+        }
+    }
+}
