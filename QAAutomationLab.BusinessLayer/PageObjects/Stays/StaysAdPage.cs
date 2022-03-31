8caef2e3da4802ade4fe53dbcb16a61fcdcc86e7
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-03-31 12:53:26 +0300

    Implement navigating to Ad Page on Stays Search Results page

---
+++ QAAutomationLab.BusinessLayer/PageObjects/Stays/StaysAdPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.BasePage;
+using QAAutomationLab.CoreLayer.WebElement;
+using System;
+
+namespace QAAutomationLab.BusinessLayer.PageObjects.Stays
+{
+    public class StaysAdPage : BasePage
+    {
+        private BaseWebElement _titleElement = new(By.XPath("//title"));
+
+        private BaseWebElement _hotelNameElement = new(By.Id("hp_hotel_name"));
+
+        private BaseWebElement _reserveButton = new(By.Id("hp_book_now_button"));
+
+        public StaysAdPage() : base()
+        {
+            try
+            {
+                var str = _hotelNameElement.Text;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message + "\n Driver hasn't navigated to Ad page");
+            }
+        }
+
+        public string GetHotelName() => _hotelNameElement.Text;
+    }
+}
--- QAAutomationLab.BusinessLayer/PageObjects/Stays/StaysSearchResultsPage.cs
+++ QAAutomationLab.BusinessLayer/PageObjects/Stays/StaysSearchResultsPage.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
+using QAAutomationLab.BusinessLayer.Utilities;
 using QAAutomationLab.CoreLayer.BasePage;
+using QAAutomationLab.CoreLayer.WebElement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,41 @@ namespace QAAutomationLab.BusinessLayer.PageObjects.Stays
     {
         private const string _title = "Hotels in";
 
+        private const string _noAdsExceptionMessage = "There are no ads on the page";
+
+        private BaseWebElement AdsHeadElement => new(By.XPath("//h1"));
+
+        private BaseWebElement FirstAdNavigatingButton
+            => new(DriverInstance.FindElements(By.XPath("//div[@data-testid='property-card']//a[@role='button']")).FirstOrDefault());
+
+        private BaseWebElement FirstAdTitle
+            => new(DriverInstance.FindElements(By.XPath("//div[@data-testid = 'title']")).FirstOrDefault());
+
         public StaysSearchResultsPage() : base()
         {
             DriverInstance.FindElement(By.XPath($"//title[contains(text(),'{_title}')]"));
+        }
+
+        public int? GetAdsCount()
+        {
+            string digits = new String(AdsHeadElement.Text.Where(c => Char.IsDigit(c)).ToArray());
+            if (Int32.TryParse(digits, out int number))
+                return number;
+            else
+                return null;
+        }
+
+        public string GetFirstAddTitle()
+            => FirstAdTitle.Text;
+
+        public StaysAdPage ClickFirstAdNavigatingButton()
+        {
+            if (GetAdsCount() > 0)
+            { 
+                Utilities.Utilities.SwitchToNewHandle(FirstAdNavigatingButton.Click);
+                return new StaysAdPage();
+            }
+            throw new ArgumentException(_noAdsExceptionMessage);
         }
     }
 }
