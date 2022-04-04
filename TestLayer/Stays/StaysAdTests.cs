e5ee92f2362c18db3a6f26a29318306637d01856
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-04 09:51:55 +0300

    Create test on navigating to stays booking page

---
+++ TestLayer/Stays/StaysAdTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.Models;
+using QAAutomationLab.BusinessLayer.PageObjects.Stays;
+using QAAutomationLab.BusinessLayer.Utilities;
+using System.Threading;
+
+namespace TestLayer.Stays
+{
+    [TestFixture]
+    [Category("All")]
+    public class StaysAdTests : BaseTest
+    {
+        private StaysAdPage _page;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                             .GoToStays()
+                             .AddSearchingContext(StaysSearchingContext.GetDefaultContext())
+                             .ClickSearchButton()
+                             .ClickFirstAdNavigatingButton();
+        }
+
+        [Test]
+        [Category("Smoke")]
+        public void CorrectNavigatingToBookingPageTest()
+        {
+            string expectedHotelName = _page.GetHotelName();
+
+            string actualHotelName = _page.AddOneRoomToBooking()
+                                          .ClickNavigatingToBookingButton()
+                                          .GetHotelName();
+
+            Assert.That(actualHotelName, Is.SubsetOf(expectedHotelName));
+        }
+
+    }
+}
