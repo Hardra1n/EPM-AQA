95666205af06f12ae08ae25ce9650dadb5cda57a
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-05 10:53:21 +0300

    Add constructor on intializing container element

--- QAAutomationLab.CoreLayer/BasePage/BasePage.cs
+++ QAAutomationLab.CoreLayer/BasePage/BasePage.cs
@@ -1,14 +1,25 @@
 ﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.Logging;
+using QAAutomationLab.CoreLayer.WebElement;
+using Serilog;
 
 namespace QAAutomationLab.CoreLayer.BasePage
 {
     public abstract class BasePage
     {
+        protected BaseWebElement ContainerElement;
+
         public IWebDriver DriverInstance;
 
         public BasePage()
         {
             this.DriverInstance = Driver.Driver.GetInstance();           
+        }
+
+        public BasePage(By containerLocator)
+        {
+            DriverInstance = Driver.Driver.GetInstance();
+            ContainerElement = new(containerLocator);
         }
     }
 }
