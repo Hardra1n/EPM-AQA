53c5ff5a3dcfd793ac65dca066446449e2ed07e7
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-12 15:05:20 +0300

    Refactoring StaysAdPage

--- QAAutomationLab.BusinessLayer/PageObjects/Stays/StaysAdPage.cs
+++ QAAutomationLab.BusinessLayer/PageObjects/Stays/StaysAdPage.cs
@@ -1,55 +1,12 @@
-﻿using System;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+﻿using QAAutomationLab.BusinessLayer.PageObjects.Stays.Components;
 using QAAutomationLab.CoreLayer.BasePage;
-using QAAutomationLab.CoreLayer.WebElement;
 
 namespace QAAutomationLab.BusinessLayer.PageObjects.Stays
 {
     public class StaysAdPage : BasePage
     {
-        public StaysAdPage()
-            : base()
-        {
-            try
-            {
-                var str = _hotelNameElement.Text;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message + "\n Driver hasn't navigated to Ad page");
-            }
-        }
+        public StaysAdHeaderContainer HeaderContainer => new();
 
-        private BaseWebElement _hotelNameElement => new(By.Id("hp_hotel_name"));
-
-        private BaseWebElement _mapContainer => new(By.Id("b_map_container"));
-
-        private BaseWebElement _firstRoomSelector
-            => new(By.XPath("//select[contains(@data-component, 'select-rooms')]"));
-
-        private BaseWebElement _navigatingToBookingButton
-            => new(By.XPath("//div[contains(@data-component, 'reservation-cta')]/button"));
-
-        public string GetHotelName()
-        {
-            int hotelNameStartIndex = _hotelNameElement.Text.LastIndexOf('\n') + 1;
-            return _hotelNameElement.Text.Substring(hotelNameStartIndex);
-        }
-
-        public bool IsMapDisplayed() => _mapContainer.Displayed;
-
-        public StaysAdPage AddOneRoomToBooking()
-        {
-            SelectElement element = new SelectElement(_firstRoomSelector.Element);
-            element.SelectByValue("1");
-            return this;
-        }
-
-        public StaysBookingDetailsPage ClickNavigatingToBookingButton()
-        {
-            _navigatingToBookingButton.Click();
-            return new StaysBookingDetailsPage();
-        }
+        public StaysAdAvailableRoomContainer RoomContainer => new();
     }
 }
