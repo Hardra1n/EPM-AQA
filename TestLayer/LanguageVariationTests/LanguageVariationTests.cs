9a8739fa4f04d8699d280cf94bcf7c19e75b1a86
EugenTrifonov <trifonov-15@mail.ru>
2022-04-18 12:54:00 +0300

    Add language variation test

---
+++ TestLayer/LanguageVariationTests/LanguageVariationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.Utilities;
+
+namespace TestLayer.LanguageVariationTests
+{
+    [TestFixture]
+    public class LanguageVariationTests : BaseTest
+    {
+        [Test]
+        [TestCase("Deutsch")]
+        public void LanguageSelectionTest(string language)
+        {
+            var page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                .MainPageTopBar;
+
+            string defaultText = page.GetCarRentalsButtonsText();
+
+            string changedText = page.ClickLanguageSelectionButton().ChooseLanguage(language).MainPageTopBar.GetCarRentalsButtonsText();
+
+            Assert.IsFalse(defaultText == changedText);
+        }
+    }
+}
