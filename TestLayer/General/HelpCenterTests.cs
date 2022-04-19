9833ed85cff2e97d4d1dddb6a08f8ccd15f38125
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-19 11:27:28 +0300

    Create test on help center validating question buttons

---
+++ TestLayer/General/HelpCenterTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.PageObjects.HelpCenter;
+using QAAutomationLab.BusinessLayer.PageObjects.HelpCenter.Components;
+using QAAutomationLab.BusinessLayer.Utilities;
+
+namespace TestLayer.General
+{
+    [TestFixture]
+    [Category("All")]
+    public class HelpCenterTests : BaseTest
+    {
+        private HelpCenterPage _page;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                .MainPageTopBar
+                .GoToHelpCenter();
+        }
+
+        [Test]
+        public void QuestionsNavigatingButtonsDisplayableAndEnabled()
+        {
+            var topics = _page.PopularQuestionsContainer.GetQuestionTopics();
+
+            Assert.Multiple(() =>
+            {
+                foreach (var topic in topics)
+                {
+                    var questionButtons = topic.GetQuestionsButtons();
+                    foreach (var button in questionButtons)
+                    {
+                        Assert.IsTrue(button.Displayed && button.Enabled);
+                    }
+                }
+            });
+        }
+    }
+}
