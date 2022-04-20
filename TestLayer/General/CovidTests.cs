d10e0d51e9f02b24e9c66cb8f5aa6748e5d80d0d
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-20 10:09:42 +0300

    Create base structure of covid tests

---
+++ TestLayer/General/CovidTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.PageObjects.Covid19;
+using QAAutomationLab.BusinessLayer.Utilities;
+
+namespace TestLayer.General
+{
+    [TestFixture]
+    [Category("All")]
+    public class CovidTests : BaseTest
+    {
+        private CovidPage _page;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                             .MainPageTopBar
+                             .GoToStays()
+                             .MainContent
+                             .GoToCovidPage();
+        }
+    }
+}
