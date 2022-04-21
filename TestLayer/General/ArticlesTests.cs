9546cd942e4c25cc91a3ab003fedec000c6c66ab
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-21 10:39:31 +0300

    Create test on checking correct link at main page

---
+++ TestLayer/General/ArticlesTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.Utilities;
+
+namespace TestLayer.General
+{
+    [TestFixture]
+    [Category("All")]
+    public class ArticlesTests : BaseTest
+    {
+        [Test]
+        public void ArticlesOnMainPageHasCorrectLinks()
+        {
+            string correctStartingSubstring
+                = "https://www.booking.com/articles/";
+            var page = Utilities.RunBrowser(TestsSettings.MainPageUrl)
+                .MainPageTopBar
+                .GoToStays()
+                .MainContent;
+
+            var links = page.GetArticles();
+
+            Assert.Multiple(() =>
+            {
+                foreach (var link in links)
+                {
+                    string hrefAttribute = link.GetAttribute("href");
+                    Assert.That(hrefAttribute, Does.StartWith(correctStartingSubstring));
+                }
+            });
+        }
+    }
+}
