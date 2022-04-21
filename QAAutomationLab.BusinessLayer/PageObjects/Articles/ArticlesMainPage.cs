5793ddaf3fb5bd415d9d9c9afd8c6730617213e3
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-04-21 11:44:16 +0300

    Create structure to working with articles pages

--- QAAutomationLab.BusinessLayer/PageObjects/Articles/ArticlesMainPage.cs
+++ QAAutomationLab.BusinessLayer/PageObjects/Articles/ArticlesMainPage.cs
@@ -1,8 +1,19 @@
-﻿using QAAutomationLab.CoreLayer.BasePage;
+﻿using QAAutomationLab.BusinessLayer.PageObjects.Articles.Components;
+using QAAutomationLab.CoreLayer.BasePage;
 
 namespace QAAutomationLab.BusinessLayer.PageObjects.Articles
 {
     public class ArticlesMainPage : BasePage
     {
+        public ArticlesMainPage()
+            : base()
+        {
+            TopicsContainer = new ArticlesTopicsContainer();
+            ArticlesContainer = new ArticlesContainer();
+        }
+
+        public ArticlesTopicsContainer TopicsContainer { get; private set; }
+
+        public ArticlesContainer ArticlesContainer { get; private set; }
     }
 }
---
+++ QAAutomationLab.BusinessLayer/PageObjects/Articles/Components/ArticleCard.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using QAAutomationLab.CoreLayer.BasePage;
+using QAAutomationLab.CoreLayer.WebElement;
+
+namespace QAAutomationLab.BusinessLayer.PageObjects.Articles.Components
+{
+    public class ArticleCard : BasePage
+    {
+        public ArticleCard(BaseWebElement containerElement)
+        {
+            this.containerElement = containerElement;
+        }
+
+        private BaseWebElement _articleLink
+            => containerElement.FindElement(By.XPath(".//a"));
+
+        public string GetArticleTitle() => _articleLink.Text;
+
+        public ArticlePage GoToArticle()
+        {
+            _articleLink.Click();
+            return new ArticlePage();
+        }
+    }
+}
