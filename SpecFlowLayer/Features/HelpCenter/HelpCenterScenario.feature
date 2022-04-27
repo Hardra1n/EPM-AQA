2b07b6a0ce925c3b9fff28639d754a890e9472a7
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-04-27 12:46:55 +0300

    Added scenario to help center page

---
+++ SpecFlowLayer/Features/HelpCenter/HelpCenterScenario.feature
@@ -0,0 +1,7 @@
+﻿Feature: HelpCenterScenario
+
+@browser
+Scenario: Navigating buttons displayable and enabled
+	Given I go to help center
+	When I get question topics
+	Then All buttons in topics are enabled and displayed
---
+++ SpecFlowLayer/StepDefinitions/HelpCenterStepDefinitions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QAAutomationLab.BusinessLayer.PageObjects.HelpCenter;
+using QAAutomationLab.BusinessLayer.PageObjects.HelpCenter.Components;
+using QAAutomationLab.BusinessLayer.Utilities;
+using QAAutomationLab.CoreLayer.BasePage;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowLayer.StepDefinitions
+{
+    [Binding]
+    public sealed class HelpCenterStepDefinitions
+    {
+        private HelpCenterPage _page;
+
+        private IEnumerable<QuestionTopicContainer> _topics;
+
+        [Given(@"I go to help center")]
+        public void GoToHelpCenterPage()
+        {
+            _page = Utilities.RunBrowser("https://www.booking.com/index.en-gb.html").MainPageTopBar.GoToHelpCenter();
+        }
+
+        [When(@"I get question topics")]
+        public void GetPopularTopics()
+        {
+            _topics = _page.PopularQuestionsContainer.GetQuestionTopics();
+        }
+
+        [Then(@"All buttons in topics are enabled and displayed")]
+        public void AssertThatAllTopicsHasEnabledButtons()
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var topic in _topics)
+                {
+                    var questionButtons = topic.GetQuestionsButtons();
+                    foreach (var button in questionButtons)
+                    {
+                        Assert.IsTrue(button.Displayed && button.Enabled);
+                    }
+                }
+            });
+        }
+    }
+}
