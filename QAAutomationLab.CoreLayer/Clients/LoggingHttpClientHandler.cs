0bda50dd786e04f011a6a245b868e6c3d5ca5dd1
Van Kiryl Kho <Van_Kiryl_Kho@epam.com>
2022-05-06 14:45:16 +0300

    Change implementation of logging message handler

--- QAAutomationLab.CoreLayer/Clients/LoggingHttpClientHandler.cs
+++ QAAutomationLab.CoreLayer/Clients/LoggingHttpClientHandler.cs
@@ -19,30 +19,11 @@ namespace QAAutomationLab.CoreLayer.Clients
                 $"{request.RequestUri}. " +
                 $"Response id: {response.Id}");
 
-            _ = RegisterResponse(response);
+            await response;
+            _logger.Logger.Information($"Response {response.Id} result: {response.Result}");
+            _logger.Logger.Information($"Response {response.Id} status: {response.Status}");
 
             return await response;
-        }
-
-        private async Task RegisterResponse(Task<HttpResponseMessage> response)
-        {
-            await response;
-            _logger.Logger.Information($"Response with id {response.Id} result: {response.Result}");
-            switch (response.Status)
-            {
-                case TaskStatus.RanToCompletion:
-                    _logger.Logger.Information($"Response with id {response.Id} completed");
-                    break;
-                case TaskStatus.Canceled:
-                    _logger.Logger.Information($"Response with id {response.Id} cancelled");
-                    break;
-                case TaskStatus.Faulted:
-                    _logger.Logger.Error($"Response with id {response.Id} faulted");
-                    break;
-                default:
-                    _logger.Logger.Error($"Unrecognized response status. Response id {response.Id}");
-                    break;
-            }
         }
     }
 }
