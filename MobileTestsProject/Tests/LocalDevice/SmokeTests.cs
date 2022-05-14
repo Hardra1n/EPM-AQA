84ccaeb67b2c1072974974e007c437ebe8f824fd
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-05-14 13:01:57 +0300

    Configuring project, creating first test

---
+++ MobileTestsProject/Tests/LocalDevice/SmokeTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace MobileTestsProject.Tests.LocalDevice
+{
+    [TestFixture]
+    public class SmokeTests : BaseTest
+    {
+        [Test]
+        public void GoToAttractions_WhenAllDataAreValid_ShouldReturnAttractionPage()
+        {
+            // Act
+            Driver.Navigate().GoToUrl("https://www.booking.com/index.en-gb.html");
+        }
+    }
+}
